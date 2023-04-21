5534c6d9bfe160d73736890b52b6efbc8a2fdc77
“算子” <1687712633@qq.com>
2023-04-21 23:14:41 +0800

    feat:完成dashboard结构搭建

--- src/views/DashboardPage/views/DataOverview/index.vue
+++ src/views/DashboardPage/views/DataOverview/index.vue
@@ -19,28 +19,196 @@
     <!-- 卡片组 -->
     <div class="card-group">
       <!-- 单独卡片 -->
-      <div class="date-card" v-for="i in 4">
+      <div class="data-card">
         <div class="card-left-icon">
-
+          <el-icon>
+            <Flag />
+          </el-icon>
         </div>
         <div class="card-right-info">
-
+          <p class="info-title">
+            {{ 6 }}支队伍
+          </p>
+          <p class="info-p">
+            队伍数
+          </p>
+        </div>
+      </div>
+      <div class="data-card">
+        <div class="card-left-icon">
+          <el-icon>
+            <User />
+          </el-icon>
+        </div>
+        <div class="card-right-info">
+          <p class="info-title">
+            {{ 20 }}名球员
+          </p>
+          <p class="info-p">
+            球员人数
+          </p>
+        </div>
+      </div>
+      <div class="data-card">
+        <div class="card-left-icon">
+          <el-icon>
+            <Histogram />
+          </el-icon>
+        </div>
+        <div class="card-right-info">
+          <p class="info-title">
+            {{ 5 }}个训练计划
+          </p>
+          <p class="info-p">
+            训练计划
+          </p>
+        </div>
+      </div>
+      <div class="data-card">
+        <div class="card-left-icon">
+          <el-icon>
+            <List />
+          </el-icon>
+        </div>
+        <div class="card-right-info">
+          <p class="info-title">
+            {{ 10 }}次训练
+          </p>
+          <p class="info-p">
+            训练次数
+          </p>
         </div>
       </div>
     </div>
     <!-- 图表组 -->
     <div class="chart-group">
-      <div class="chat-item" v-for="i in 2">
+      <!-- 训练计划分布 -->
+      <div class="chat-item">
+        <!-- 头部 -->
+        <section class="title-info">
+          <div class="left-info">
+            <p class="info-title">
+              训练计划分布
+            </p>
+            <p class="info-p">
+              总览训练计划分布
+            </p>
+          </div>
+          <div class="right-filter">
+            <div class="filter-wrap">
+              <div class="filter-item filter-item-active">
+                Monthly
+              </div>
+              <div class="filter-item">
+                Weekly
+              </div>
+              <div class="filter-item">
+                Today
+              </div>
+            </div>
+          </div>
+        </section>
+        <!-- 方块字段 -->
+        <div class="chat-item-info-box">
+          <div class="chat-item-info">
+            <p class="info-title">
+              {{ 6 }}次
+            </p>
+            <p class="info-p">
+              体能训练
+            </p>
+          </div>
+          <div class="chat-item-info">
+            <p class="info-title">
+              {{ 6 }}次
+            </p>
+            <p class="info-p">
+              专项训练
+            </p>
+          </div>
+          <div class="chat-item-info">
+            <p class="info-title">
+              {{ 6 }}次
+            </p>
+            <p class="info-p">
+              训练赛
+            </p>
+          </div>
+        </div>
+        <!-- 图表字段 -->
+        <div class="chat-item-box">
+          <!-- 左侧饼图 -->
+          <div class="chat-item-left-chat">
+          </div>
+          <!-- 右侧柱状图 -->
+          <div class="chat-item-right-chat">
+          </div>
+        </div>
+      </div>
+      <!-- 训练数据 -->
+      <div class="chat-item">
+        <section class="title-info">
+          <div class="left-info">
+            <p class="info-title">
+              训练数据
+            </p>
+            <p class="info-p">
+              训练数据预览
+            </p>
+          </div>
+          <div class="right-filter">
+            <div class="filter-wrap">
+              <div class="filter-item filter-item-active">
+                Monthly
+              </div>
+              <div class="filter-item">
+                Weekly
+              </div>
+              <div class="filter-item">
+                Today
+              </div>
+            </div>
+          </div>
+        </section>
+        <!-- 图表字段 -->
+        <div class="chat-item-box-2">
 
+        </div>
       </div>
     </div>
     <!-- 底部图 -->
     <div class="buttom-chat-group">
       <div class="bar-chat">
-
+        <section class="title-info">
+          <div class="left-info">
+            <p class="info-title">
+              训练数据
+            </p>
+            <p class="info-p">
+              训练数据预览
+            </p>
+          </div>
+          <div class="right-filter">
+            <div class="filter-wrap">
+              <div class="filter-item filter-item-active">
+                Monthly
+              </div>
+              <div class="filter-item">
+                Weekly
+              </div>
+              <div class="filter-item">
+                Today
+              </div>
+            </div>
+          </div>
+        </section>
+        <div class="chat-item-box">
+        </div>
       </div>
       <div class="more-info-card">
+        <div class="more-info-box">
 
+        </div>
       </div>
     </div>
   </main>
@@ -49,8 +217,15 @@
  
 <script lang='ts' setup>
 import { ref, reactive } from 'vue'
+import {
+  User, Histogram, List, Flag, ArrowDown
+} from '@element-plus/icons-vue'
+//左上角饼图和柱状图
 
 
+//右侧面积图
+
+//下部住柱状图
 </script>
  
 <style scoped lang="less">
@@ -83,15 +258,39 @@ import { ref, reactive } from 'vue'
     flex-wrap: nowrap;
     align-items: center;
 
-    .date-card {
+    .data-card {
       width: 280px;
       height: 150px;
       background-color: #fff;
       border-radius: 5%;
-      box-shadow: 0 2px 4px rgba(0, 0, 0, .25);
+      box-shadow: 0 1px 1px rgba(0, 0, 0, .25);
+      display: flex;
+      align-items: center;
+      position: relative;
 
       .card-left-icon {
-        height: 50px;
+        background: #E9ECFF;
+        height: 60px;
+        width: 60px;
+        border-radius: 50%;
+        margin: 0 30px;
+        margin-right: 20px;
+        color: #2F4CDD;
+        font-size: 30px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+
+      .card-right-info {
+        .info-title {
+          font-size: 20px;
+          font-weight: 600;
+        }
+
+        .info-p {
+          color: #969BA0;
+        }
       }
     }
   }
@@ -108,8 +307,115 @@ import { ref, reactive } from 'vue'
       height: 400px;
       background-color: #fff;
       border-radius: 5%;
-      box-shadow: 0 2px 4px rgba(0, 0, 0, .25);
+      box-shadow: 0 1px 1px rgba(0, 0, 0, .25);
+      box-sizing: border-box;
+      padding: 40px 30px;
+
+      .title-info {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        .left-info {
+          .info-title {
+            font-size: 20px;
+            font-weight: 600;
+          }
+
+          .info-p {
+            color: #969BA0;
+          }
+        }
+
+        .right-filter {
+          .filter-wrap {
+            height: 50px;
+            background: #F4F5F9;
+            border-radius: 8px;
+            display: flex;
+            align-items: center;
+            justify-content: space-around;
+
+            .filter-item {
+              height: 30px;
+              line-height: 30px;
+              flex: 1;
+              text-align: center;
+              margin: 0 10px;
+              border-radius: 4px;
+              padding: 2px 16px;
+              color: rgba(0, 0, 0, 0.6);
+              cursor: pointer;
+            }
+
+            .filter-item-active {
+              background: #fff;
+              box-shadow: 0 1px 0 rgb(0 0 0 / 10%), 0 0 0 1px rgb(0 0 0 / 2%),
+                0 5px 10px rgb(0 0 0 / 15%);
+              color: #000;
+            }
+          }
+        }
+      }
+
+      .chat-item-info-box {
+        margin-top: 10px;
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: nowrap;
+        align-items: center;
+
+        .chat-item-info {
+          width: 170px;
+          height: 80px;
+          background-color: #fff;
+          border-radius: 5%;
+          border: 1px solid #F2F2F2;
+          box-sizing: border-box;
+          padding: 10;
+          box-sizing: border-box;
+          padding: 15px 0 0px 20px;
+
+          .info-title {
+            font-size: 20px;
+            font-weight: 600;
+          }
+
+          .info-p {
+            color: #969BA0;
+          }
+        }
+      }
+
+      .chat-item-box {
+        margin-top: 20px;
+        box-sizing: border-box;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+        .chat-item-left-chat {
+          border: 1px solid red;
+          height: 175px;
+          width: 40%;
+        }
+
+        .chat-item-right-chat {
+          border: 1px solid rgb(61, 33, 187);
+          height: 175px;
+          width: 60%;
+        }
+      }
+
+      .chat-item-box-2 {
+        margin-top: 20px;
+        box-sizing: border-box;
+        border: 1px solid red;
+        height: 270px;
+        width: 100%;
+      }
     }
+
   }
 
   .buttom-chat-group {
@@ -119,20 +425,85 @@ import { ref, reactive } from 'vue'
     flex-wrap: nowrap;
     align-items: center;
 
-    .bar-chat {
+    .bar-chat,
+    .more-info-card {
+      box-sizing: border-box;
       width: 70%;
-      min-height: 300px;
+      min-height: 400px;
       background-color: #fff;
       border-radius: 10px;
-      box-shadow: 0 2px 4px rgba(0, 0, 0, .25);
+      box-shadow: 0 1px 1px rgba(0, 0, 0, .25);
+      padding: 40px 30px;
+      padding-bottom: 30px;
+
+      .title-info {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        .left-info {
+          .info-title {
+            font-size: 20px;
+            font-weight: 600;
+          }
+
+          .info-p {
+            color: #969BA0;
+          }
+        }
+
+        .right-filter {
+          .filter-wrap {
+            height: 50px;
+            background: #F4F5F9;
+            border-radius: 8px;
+            display: flex;
+            align-items: center;
+            justify-content: space-around;
+
+            .filter-item {
+              height: 30px;
+              line-height: 30px;
+              flex: 1;
+              text-align: center;
+              margin: 0 10px;
+              border-radius: 4px;
+              padding: 2px 16px;
+              color: rgba(0, 0, 0, 0.6);
+              cursor: pointer;
+            }
+
+            .filter-item-active {
+              background: #fff;
+              box-shadow: 0 1px 0 rgb(0 0 0 / 10%), 0 0 0 1px rgb(0 0 0 / 2%),
+                0 5px 10px rgb(0 0 0 / 15%);
+              color: #000;
+            }
+          }
+        }
+      }
+
+      .chat-item-box {
+        margin-top: 20px;
+        box-sizing: border-box;
+        border: 1px solid red;
+        height: 270px;
+        width: 100%;
+      }
     }
 
     .more-info-card {
       width: 28%;
-      min-height: 300px;
-      background-color: #fff;
-      border-radius: 10px;
-      box-shadow: 0 2px 4px rgba(0, 0, 0, .25);
+      padding: 40px 30px;
+      padding-bottom: 30px;
+      min-height: 410px;
+      .more-info-box {
+        margin-top: 20px;
+        box-sizing: border-box;
+        border: 1px solid red;
+        height: 320px;
+        width: 100%;
+      }
     }
 
   }
--- src/views/DashboardPage/views/TeamManagement/index.vue
+++ src/views/DashboardPage/views/TeamManagement/index.vue
@@ -1,16 +1,86 @@
 <template>
-  <div>
-    球队管理
-  </div>
+  <main class="team-manage">
+    <!-- 文字说明 -->
+    <section class="title-info">
+      <div class="left-info">
+        <p class="info-title">
+          队伍管理
+        </p>
+        <p class="info-p">
+          在此可查看球员信息和管理球员
+        </p>
+      </div>
+      <!-- 在这可设置添加新球员信息等 -->
+      <div class="right-filter">
+        <div>
+        </div>
+      </div>
+    </section>
+    <!-- 球员信息 -->
+    <div class="team-info-box">
+      <!-- 左侧球员列表 -->
+      <div class="team-list">
+
+      </div>
+      <!-- 右侧球员信息 -->
+      <div class="team-palyer-info">
+
+      </div>
+    </div>
+  </main>
 </template>
  
  
 <script lang='ts' setup>
-import { ref,reactive } from 'vue'
- 
- 
+import { ref, reactive } from 'vue'
+
+
 </script>
  
-<style scoped>
- 
+<style scoped lang="less">
+.team-manage {
+  min-height: 600px;
+  padding: 40px 30px;
+  padding-bottom: 0px;
+
+  .title-info {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    .left-info {
+      .info-title {
+        font-size: 20px;
+        font-weight: 600;
+      }
+
+      .info-p {
+        color: #969BA0;
+      }
+    }
+  }
+
+  .team-info-box {
+    margin-top: 30px;
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: nowrap;
+    align-items: flex-start;
+
+    .team-list {
+      width: 40%;
+      height: 590px;
+      overflow: auto;
+      background-color: #fff;
+      box-shadow: 0 2px 4px rgba(0, 0, 0, .25);
+    }
+
+    .team-palyer-info {
+      width: 58%;
+      min-height: 590px;
+      background-color: #fff;
+      box-shadow: 0 2px 4px rgba(0, 0, 0, .25);
+    }
+  }
+}
 </style>
